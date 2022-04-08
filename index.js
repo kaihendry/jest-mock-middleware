1dcb26da096d2a11450c43378b15987d04d8873a
Kai Hendry <hendry@iki.fi>
2022-04-08 10:39:24 +0800

    Test actually fails when I fetchData https://github.com/middyjs/middy/tree/main/packages/secrets-manager

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ async function baseHandler() {
   log.info("hihi", { Timestamp: new Date().getTime(), nothing: undefined });
 }
 
-const start = middy(baseHandler).use(secretsManager());
+const start = middy(baseHandler).use(
+  secretsManager({
+    fetchData: {
+      SECRETS: "/so/so/secret",
+    },
+    setToContext: true,
+  })
+);
 
 module.exports = { start };
