e4f183bf9d5834926b904c0f1964206c7d1fcdf5
Kai Hendry <hendry@iki.fi>
2022-04-08 11:13:05 +0800

    Not working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const log = require("lambda-log");
 const middy = require("@middy/core");
 const secretsManager = require("@middy/secrets-manager");
+const SecretsManager = require("aws-sdk/clients/secretsmanager.js"); // v2
 
 async function baseHandler() {
   log.info("hihi", { Timestamp: new Date().getTime(), nothing: undefined });
@@ -8,6 +9,7 @@ async function baseHandler() {
 
 const start = middy(baseHandler).use(
   secretsManager({
+    AwsClient: SecretsManager,
     fetchData: {
       SECRETS: "/so/so/secret",
     },
--- index.test.js
+++ index.test.js
@@ -1,13 +1,22 @@
 jest.mock("aws-sdk");
 
-const SecretsManager = require("aws-sdk/clients/secretsmanager");
+const SecretsManager = require("aws-sdk/clients/secretsmanager.js"); // v2
 
 const main = require("./index.js");
 const log = require("lambda-log");
 
+const mockService = (client) => {
+  // aws-sdk v2
+  const mock = jest.fn();
+  client.prototype.getSecretValue = mock;
+  return mock;
+};
+
 test("main", async () => {
-  const getSecretValueMock = jest.fn();
-  SecretsManager.prototype.getSecretValue = getSecretValueMock;
+  mockService(SecretsManager, {
+    SecretString: "token",
+  });
+
   const logSpy = jest.spyOn(log, "info").mockImplementation();
   await main.start();
   expect(logSpy).toBeCalledWith("hihi", {
