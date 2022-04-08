969a36876b5e04e20bc47dde23390c0d09f79108
Kai Hendry <hendry@iki.fi>
2022-04-08 09:22:20 +0800

    Clean up

--- index.test.js
+++ index.test.js
@@ -4,13 +4,11 @@ const SecretsManager = require("aws-sdk/clients/secretsmanager");
 
 const main = require("./index.js");
 const log = require("lambda-log");
-const secretsManager = require("@middy/secrets-manager");
 
 test("main", async () => {
   const getSecretValueMock = jest.fn();
   SecretsManager.prototype.getSecretValue = getSecretValueMock;
   const logSpy = jest.spyOn(log, "info").mockImplementation();
-  // mock secretsManager middleware to do nothing
   await main.start();
   expect(logSpy).toBeCalledWith("hihi", {
     Timestamp: expect.any(Number),
